dd9fc3c43f0dfea9fc23e9bdd0f9cc7d9d1895ce
eylon696 <73744890+eylon696@users.noreply.github.com>
2020-11-01 23:48:29 +0200

    Add files via upload

---
+++ lab 12/main.c
@@ -0,0 +1,56 @@
+#include "ex.h"
+
+
+
+  
+
+
+
+
+
+
+
+  int main()
+  {
+    char ch,*temp;
+    int n,i;
+    BOOL res;
+    PNode head=NULL,tail=NULL;
+	
+    printf("Enter number of characters ");
+    scanf("%d",&n);            
+    printf("\nEnter %d characters,separated by enter",n);
+    for(i=0;i<n;i++) 
+    {
+        temp=(char*)malloc(sizeof(char));
+        if (NULL==temp)
+	{
+	    printf("\nCan't allocate data memory");
+	    freeAll(&head,free_char);
+	    return 1;
+        } 
+        scanf(" %c",temp);
+        res=insertEntry(&head,&tail,temp,cmp_char);
+	if(FALSE==res)  /* if FALSE==res than there is something wrong with malloc */
+	{							
+	    printf("\nCan't allocate node memory or data is allready exists");
+	    freeAll(&head,free_char);
+	    return 1;
+	}
+     }  
+     printAll(head,prnt_char); 
+
+     printf("\nEnter an element for deleting");
+     scanf(" %c",&ch);
+     res=deleteNode(&head,&tail,&ch,cmp_char,free_char);
+     if(FALSE==res)   
+        printf("%c don't exists in list\n",ch);
+
+     printAll(head,prnt_char);
+
+     freeAll(&head,free_char);
+     printf("\nYour list was destroyed\n");
+	
+     return 0;
+}
+  
